76132b680c2691fed62b17eb9d469da469f32422
alexanderjervis <alexander.jervis@revature.net>
2021-06-07 17:20:59 -0400

    fixed test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-py p0.py Buck 2 5
-py p0.py Duck 2 5 3 2
-py p0.py Lucky 6 4 2
-py p0.py Muck 7 5 3
-py p0.py Puck 1 2 3
-py p0.py 123 4 5 s
+python3 p0.py Buck 2 5
+python3 p0.py Duck 2 5 3 2
+python3 p0.py Lucky 6 4 2
+python3 p0.py Muck 7 5 3
+python3 p0.py Puck 1 2 3
+python3 p0.py 123 4 5 s
