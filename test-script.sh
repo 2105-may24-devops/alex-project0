d65a9ab6575198782354c857c6aa53b0a6f17c2a
alexanderjervis <alexander.jervis@revature.net>
2021-06-09 02:30:37 -0400

    added requirements.txt and test scripts

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
-python3 p0.py Buck 2 5
-python3 p0.py Duck 2 5 3 2
-python3 p0.py Lucky 6 4 2
-python3 p0.py Muck 7 5 3
-python3 p0.py Puck 1 2 3
-python3 p0.py 123 4 5 s
+{ python3 p0.py uno 2 5 
+python3 p0.py dos 2 5 3 2 
+python3 p0.py tres 6 4 2 
+python3 p0.py quatro 7 5 3 
+python3 p0.py cinco 1 2 3 
+python3 p0.py test6 4 5 s 
+} > test-run.txt
+
+if cmp "test-run.txt" "test-cmp.txt"; then
+      echo "Tests succeeded"
+    else
+      echo "Tests failed"
+    fi
